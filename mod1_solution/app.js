cbf8a5b1a81c8896252750f6ab64b60c397a86d4
Jeremy Feng <jeremy.jfeng@gmail.com>
2016-09-18 23:54:41 +1000

    final submission

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -5,18 +5,34 @@
   .controller('LunchCheckController', LunchCheckController);
 
   LunchCheckController.$inject = ['$scope'];
-  funciton LunchCheckController($scope) {
+  function LunchCheckController($scope) {
     $scope.message = "";
     $scope.lunches = "";
+    $scope.status = "";
     $scope.check = function() {
       var lunches = $scope.lunches;
       if(lunches) {
         var arrLunches = lunches.split(',');
+        arrLunches = arrLunches.filter(function(n) { return n.trim() != "" });
         var num = arrLunches.length;
-        if(num <= 3) $scope.message = "Enjoy!";
-        else if(num > 3) $scope.message = "Too much!";
+        switch (num) {
+          case 0:
+            $scope.message = "Please enter data first";
+            $scope.status = 'err';
+            break;
+          case 1:
+          case 2:
+          case 3:
+             $scope.message = "Enjoy!";
+             $scope.status = 'ok';
+             break;
+          default:
+            $scope.message = "Too much!";
+            $scope.status = 'ok';
+        }
       } else {
         $scope.message = "Please enter data first";
+        $scope.status = 'err';
       }
     };
   }
